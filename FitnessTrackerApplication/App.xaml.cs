ca7904baef34b021ef476289621d2ee48ad42f83
Rahul Lore <RahulLore@gmail.com>
2019-03-08 11:07:24 -0500

    first check-in

---
+++ FitnessTrackerApplication/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FitnessTrackerApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml asdadasd
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
