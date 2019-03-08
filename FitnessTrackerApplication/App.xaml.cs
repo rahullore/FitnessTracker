5156770c4fe6845616fbb7e9769af4caee22c86e
Rahul Lore <RahulLore@gmail.com>
2019-03-08 12:16:15 -0500

    setting up projects

--- FitnessTrackerApplication/App.xaml.cs
+++ FitnessTrackerApplication/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace FitnessTrackerApplication
 {
     /// <summary>
-    /// Interaction logic for App.xaml asdadasd
+    /// Interaction logic for App.xaml 
     /// </summary>
     public partial class App : Application
     {
